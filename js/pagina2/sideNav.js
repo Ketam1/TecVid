d9f91215cc801e73fc36ba8568f0bda642ca9061
Italo <ifvnasc@gmail.com>
2019-09-16 19:14:07 -0300

    Implementação da Side Nav

---
+++ js/pagina2/sideNav.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelector('.sidenav');
+    var instances = M.Sidenav.init(elems, {
+      edge: 'left'
+    });
+  });
